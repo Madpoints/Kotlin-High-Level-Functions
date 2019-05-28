92c25816fe2acb6ffab82dfc5bea287d4d81fa38
John Furtado <john.furtado@virginpulse.com>
2019-05-28 15:27:31 -0400

    higher order functions

--- src/rsk/Program.kt
+++ src/rsk/Program.kt
@@ -3,12 +3,19 @@ package rsk
 fun main(args: Array<String>) {
     var program = Program()
 //    program.fibonacci(8)
-    program.fibonacci(8, object: Process{
-        override fun execute(value: Int) {
-            println(value)
-        }
 
-    })
+//    program.fibonacci(8, object: Process{
+//        override fun execute(value: Int) {
+//            println(value)
+//        }
+//
+//    })
+
+//    program.fibonacci(8, {n -> println(n)})
+//    program.fibonacci(8) {n -> println(n)}
+//    program.fibonacci(8) {println(it)}
+    program.fibonacci(8, ::println)
+
 }
 
 interface Process {
@@ -16,6 +23,21 @@ interface Process {
 }
 
 class Program {
+    fun fibonacci(limit: Int, action: (Int) -> Unit) {
+        var prev = 0
+        var prevprev = 0
+        var current = 1
+
+        for (i: Int in 1..limit) {
+            action(current)
+
+            var temp = current
+            prevprev = prev
+            prev = temp
+            current = prev + prevprev
+        }
+    }
+
     fun fibonacci(limit: Int, action: Process) {
         var prev = 0
         var prevprev = 0
